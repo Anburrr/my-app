26f524c4785b424d34101c9e97b18b7f51e17052
Anburrr <96327615+Anburrr@users.noreply.github.com>
2023-05-27 12:58:14 -0400

    newCommit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './navbar.scss'
+
+function NavBar() {
+    return (
+      <div className = "overallNav">
+      <div className="navBar">
+  
+  <div class="choice1">
+          <p class = "navNum">01</p>
+          <a class = "navWords" href="">// Home</a>
+          
+      </div>
+  
+      <div class="choice2">
+          <p class = "navNum">02</p>
+          <a class = "navWords" href="">// About</a>
+      </div>
+  
+      <div class="choice3">
+          <p class = "navNum">03</p>
+          <a class = "navWords" href="">// Experience</a>
+      </div>
+  
+      <div class="choice4">
+          <p class = "navNum">04</p>
+          <a class = "navWords" href="">// Resume</a>
+      </div>
+  </div>
+      </div>
+    );
+    }
+
+    export default NavBar;
